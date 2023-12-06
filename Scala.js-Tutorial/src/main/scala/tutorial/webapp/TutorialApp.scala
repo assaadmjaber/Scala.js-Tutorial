ef5c5e38ddbb363c64e7b3f8fd73418a0cc689c0
Asaad Jaber <assaadmjaber@icloud.com>
2023-12-06 16:00:01 +0200

    added click button

--- Scala.js-Tutorial/src/main/scala/tutorial/webapp/TutorialApp.scala
+++ Scala.js-Tutorial/src/main/scala/tutorial/webapp/TutorialApp.scala
@@ -2,6 +2,7 @@ package tutorial.webapp
 
 import org.scalajs.dom
 import org.scalajs.dom.document
+import scala.scalajs.js.annotation.JSExportTopLevel
 
 object TutorialApp {
   def main(args: Array[String]): Unit = {
@@ -13,4 +14,9 @@ def appendPar(targetNode: dom.Node, text: String): Unit = {
   val parNode = document.createElement("p")
   parNode.textContent = text
   targetNode.appendChild(parNode)
+}
+
+@JSExportTopLevel("addClickedMessage")
+def addClickedMessage(): Unit = {
+  appendPar(document.body, "You clicked the button!")
 }
